b4e97bb98c8bb05b3c5bba90e8d9f177cc025dbe
OrigenesZhang <zhangbin199807@gmail.com>
2018-01-24 17:36:27 +0800

    2.5

---
+++ chapter 2/ex2.34/cmake-build-debug/CMakeFiles/ex2_34.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex2_34.dir/main.cpp.obj"
+  "ex2_34.pdb"
+  "ex2_34.exe"
+  "ex2_34.exe.manifest"
+  "libex2_34.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex2_34.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter 2/ex2.34/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+int main(){
+	int i=0, &r=i;
+	auto a=r;
+	const int ci=i, &cr=ci;
+	auto b=ci;
+	auto c=cr;
+	auto d=&i;
+	auto e=&ci;
+	const auto f=ci;
+	auto &g=ci;
+	//auto &h=42;
+	const auto &j=42;
+	auto k=ci, &l=i;
+	auto &m=ci, *p=&ci;
+	//auto &n=i, *p2=&ci;
+	std::cout<<a<<std::endl;
+	a=42;
+	std::cout<<a<<std::endl;
+	std::cout<<b<<std::endl;
+	b=42;
+	std::cout<<b<<std::endl;
+	std::cout<<c<<std::endl;
+	c=42;
+	std::cout<<c<<std::endl;
+	//taking point from integer without a cast
+	/*std::cout<<d<<std::endl;
+	d=42;
+	std::cout<<d<<std::endl;*/
+	//taking point from integer without a cast
+	/*std::cout<<e<<std::endl;
+	e=42;
+	std::cout<<e<<std::endl;*/
+	//Variable is declared constant and is not assignable
+	/*std::cout<<f<<std::endl;
+	f=42;
+	std::cout<<f<<std::endl;*/
+	return 0;
+}
