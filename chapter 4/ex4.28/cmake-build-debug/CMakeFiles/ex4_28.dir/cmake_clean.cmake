c17e921bd183154280babd536dc4a0dd4d28daba
OrigenesZhang <zhangbin199807@gmail.com>
2018-08-16 23:52:41 +0800

    4.9

---
+++ chapter 4/ex4.28/cmake-build-debug/CMakeFiles/ex4_28.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex4_28.dir/main.cpp.obj"
+  "ex4_28.pdb"
+  "ex4_28.exe"
+  "ex4_28.exe.manifest"
+  "libex4_28.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex4_28.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
