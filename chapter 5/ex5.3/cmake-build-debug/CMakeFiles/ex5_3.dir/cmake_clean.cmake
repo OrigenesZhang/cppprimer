ab8204d201110fda3898bccd7472b1b28c2df875
OrigenesZhang <zhangbin199807@gmail.com>
2018-08-21 22:45:18 +0800

    5.1

---
+++ chapter 5/ex5.3/cmake-build-debug/CMakeFiles/ex5_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex5_3.dir/main.cpp.obj"
+  "ex5_3.pdb"
+  "ex5_3.exe"
+  "ex5_3.exe.manifest"
+  "libex5_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex5_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
