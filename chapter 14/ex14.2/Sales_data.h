c0cca821caa0499b809761d59e37142194548c17
OrigenesZhang <zhangbin199807@gmail.com>
2022-06-26 22:57:47 +0800

    14.1

---
+++ chapter 14/ex14.2/Sales_data.h
@@ -0,0 +1,34 @@
+//
+// Created by Bin Zhang on 26/6/22.
+//
+
+#ifndef EX14_2_SALES_DATA_H
+#define EX14_2_SALES_DATA_H
+
+#include <string>
+#include <utility>
+#include <iostream>
+
+class Sales_data {
+public:
+  friend std::istream& operator>>(std::istream&, Sales_data&);
+  friend std::ostream& operator<<(std::ostream&, const Sales_data&);
+  friend Sales_data operator+(const Sales_data&, const Sales_data&);
+  Sales_data(std::string s, unsigned n, double p): bookNo(std::move(s)), units_sold(n), revenue(n * p) {}
+  Sales_data(): Sales_data("", 0, 0.0f) {}
+  explicit Sales_data(std::string s): Sales_data(std::move(s), 0, 0.0f) {}
+  explicit Sales_data(std::istream&);
+  Sales_data& operator+=(const Sales_data&);
+  [[nodiscard]] std::string isbn() const { return bookNo; }
+private:
+  [[nodiscard]] inline double avg_price() const;
+  std::string bookNo;
+  unsigned units_sold = 0;
+  double revenue = 0.0;
+};
+
+std::istream& operator>>(std::istream&, Sales_data&);
+std::ostream& operator<<(std::ostream&, const Sales_data&);
+Sales_data operator+(const Sales_data&, const Sales_data&);
+
+#endif //EX14_2_SALES_DATA_H
---
+++ chapter 14/ex14.2/main.cpp
@@ -0,0 +1,7 @@
+#include "Sales_data.h"
+
+int main() {
+  Sales_data item1, item2;
+  std::cin >> item1 >> item2;
+  std::cout << item1 + item2 << std::endl;
+}
